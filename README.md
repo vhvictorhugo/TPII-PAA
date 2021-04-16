b8c979d92fb4fe3e997e34ebb6e0d7b3c48ed00b
Victor Hugo <victor.h.santos@ufv.br>
2021-04-16 17:48:39 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TPII-PAA
---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+*   Programa: 
+*   Autor: Roniel Nunes Barbosa e Victor Hugo Santos
+*   Matéria: Projeto e Análise de Algoritmos
+*   Professor: Daniel Mendes Barbosa
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char valor [3] = {"+20"};
+    int valorInteiro = valor[];
+
+    if(20 == +20){
+        puts("yes");
+    }
+
+
+    return 0;
+}
